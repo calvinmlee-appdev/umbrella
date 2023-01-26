3698a846ab0493781b26011e7f6da9a602712b9d
calvinmlee <calvinmlee@uchicago.edu>
2023-01-26 07:00:03 +0000

    Finished Umbrella

--- umbrella.rb
+++ umbrella.rb
@@ -22,11 +22,68 @@ geo =  only_result.fetch("geometry")
 
 loc = geo.fetch("location")
 
-p latitude = loc.fetch("lat")
-p longitude = loc.fetch("lng")
+latitude = loc.fetch("lat")
+longitude = loc.fetch("lng")
 
 
 
 # Lat for Chic : 41.8781136
 # Long for Chic : -87.6297982
-dark_sky_url = "https://api.darksky.net/forecast/#{ENV.fetch("DARK_SKY_KEY")}/41.8781136,-87.6297982"
+dark_sky_key = ENV.fetch("DARK_SKY_KEY")
+
+dark_sky_url = "https://api.darksky.net/forecast/#{dark_sky_key}/#{latitude},#{longitude}"
+
+# p "Getting weather from:"
+# p dark_sky_url
+
+raw_dark_sky_data = URI.open(dark_sky_url).read
+
+parsed_dark_sky_data = JSON.parse(raw_dark_sky_data)
+
+currently_hash = parsed_dark_sky_data.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
+puts "It is currently #{current_temp}°F."
+
+# Some locations around the world do not come with minutely data.
+minutely_hash = parsed_dark_sky_data.fetch("minutely", false)
+
+if minutely_hash
+  next_hour_summary = minutely_hash.fetch("summary")
+
+  puts "Next hour: #{next_hour_summary}"
+end
+
+hourly_hash = parsed_dark_sky_data.fetch("hourly")
+
+hourly_data_array = hourly_hash.fetch("data")
+
+next_twelve_hours = hourly_data_array[1..12]
+
+precip_prob_threshold = 0.10
+
+any_precipitation = false
+
+next_twelve_hours.each do |hour_hash|
+
+  precip_prob = hour_hash.fetch("precipProbability")
+
+  if precip_prob > precip_prob_threshold
+    any_precipitation = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if any_precipitation == true
+  puts "You might want to take an umbrella!"
+else
+  puts "You probably won't need an umbrella."
+end
